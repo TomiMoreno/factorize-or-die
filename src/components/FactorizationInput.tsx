edf3e7423bd68da9016ed955560d0563c45437fa
Tomás Canosa Moreno <tomimoreno03@gmail.com>
2023-01-13 06:57:23 -0300

    Add spacebar to multiply

--- src/components/FactorizationInput.tsx
+++ src/components/FactorizationInput.tsx
@@ -4,10 +4,20 @@ import { useGameStore } from "../hooks/useGameStore";
 export default function FactorizationInput() {
   const factorization = useGameStore((state) => state.factorizedInput);
   const setFactorization = useGameStore((state) => state.setFactorizedInput);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let value = e.target.value;
+    // Spacebar should give '*' as input
+    value = value.replace(" ", "*");
+    // Only allow numbers and '*' as input
+    if (value.match(/^[0-9*]*$/)) {
+      setFactorization(value);
+    }
+  };
   return (
     <input
       value={factorization}
-      onChange={(e) => setFactorization(e.target.value)}
+      onChange={handleChange}
       className="border-b-2 border-gray-700 text-5xl font-extrabold leading-normal text-gray-700 focus:border-red-700 focus:outline-none md:text-[5rem]"
     />
   );
